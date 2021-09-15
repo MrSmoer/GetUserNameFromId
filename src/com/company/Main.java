0c85694f1493bc7fe1d31134c970d9946dba39fb
MrSmoer <66489839+MrSmoer@users.noreply.github.com>
2021-09-15 19:39:59 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,74 @@
+package com.company;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+import javax.security.auth.login.LoginException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Optional;
+
+public class Main {
+    public static Main INSTANCE;
+
+    public JDA jda;
+    public JDABuilder builder;
+
+    public static void main(String[] args) {
+        System.out.println("test");
+        try {
+            new Main();
+        } catch (LoginException | IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+    }
+    public Main() throws LoginException {
+        INSTANCE = this;
+
+        String TOKEN = getEnVar("BUTTER_TOKEN");
+
+        builder = JDABuilder.createLight(TOKEN);
+        jda = builder.build();
+        try {
+            jda.awaitReady();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Online");
+        startThread();
+    }
+    public void startThread(){
+        new Thread(() -> {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String line;
+            try {
+                while ((line = reader.readLine()) != null) {
+
+                    if (line.equalsIgnoreCase("exit")) {
+                        jda.shutdownNow();
+                    }
+                    jda.retrieveUserById(line).queue(usr -> {
+                        // use name here
+                        System.out.println(usr.getName());
+                    });
+                }
+            } catch (IOException | IllegalArgumentException e) {
+                e.printStackTrace();
+                startThread();
+            }
+        }).start();
+    }
+
+    public static String getEnVar(String envar) {
+        String content = null;
+        try {
+            content = Optional.ofNullable(System.getenv(envar)).orElseThrow(
+                    () -> new Exception("Environment Variable " + envar + " is not set in the environment"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return content;
+    }
+}
